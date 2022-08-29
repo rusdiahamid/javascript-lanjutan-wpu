985b0393dfcebaf9967d6c20ce1b341a256aa4e1
rusdiahamid <rusdia.hamid@gmail.com>
2022-08-29 11:36:08 +0700

    pembahasan higher order function

---
+++ script.js
@@ -0,0 +1,5 @@
+// Higher Order Function
+// adalah function yang beroperasi pada function yang lain. baik itu digunakan dalam argument maupun sebagai return value
+// Javascript memperlakukan function sebagai Object
+// alasan mengapa menggunakan HOF
+// 1. Abstraksi
